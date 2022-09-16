a210a21af537028229e3e9b6d6e9388a4a4f2930
pRostok <rostmart29@gmail.com>
2022-09-16 16:53:48 -0700

    apply styling

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const toggle = document.querySelector('.switch')
+
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark mode'
+    } else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light mode'
+    }
+})
