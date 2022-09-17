e7b1487339ea9abbac4e54f7961d827c0bb6a32d
pRostok <rostmart29@gmail.com>
2022-09-16 17:29:51 -0700

    add script

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 
+const previousTextElement = document.querySelector(".out1")
+const currentTextElement = document.querySelector(".out2")
+const numbers = document.querySelectorAll("[data-number]")
+const operators = document.querySelectorAll("[data-operation]")
+const clearAllBtn = document.querySelector("[data-all-clear]")
+const deleteBtn = document.querySelector("[data-delete]")
+const equalBtn = document.querySelector("[data-equals]")
 const toggle = document.querySelector('.switch')
 
 
@@ -11,4 +18,52 @@ toggle.addEventListener('click', (e) => {
         html.classList.add('dark')
         e.target.innerHTML = 'Light mode'
     }
+})
+
+
+
+
+clear()
+
+function clear() {
+    prevOp = ''
+    currentOp = ''
+    operation = undefined
+    currentTextElement.innerHTML = 0
+}
+
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        getNumber(number.textContent)
+        
+        console.log(number)
+        display()
+    })
+})
+
+function getNumber(number) {
+   
+    currentOp += number.toString()
+}
+
+
+function display() {
+    if (operation) {
+        previousTextElement.textContent = `${prevOp} ${operation}`
+    } else {
+        previousTextElement.textContent = `${prevOp}`
+    }
+    currentTextElement.textContent = currentOp
+}
+
+
+clearAllBtn.addEventListener("click", () => {
+    clear()
+    display()
+})
+
+
+deleteBtn.addEventListener("click", () => {
+    currentOp = currentOp.toString().slice(0, currentOp.toString().length - 1)
+    display()
 })
